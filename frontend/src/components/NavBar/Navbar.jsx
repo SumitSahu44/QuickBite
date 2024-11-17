2f8c8ee567bc8f369b9f151814c98629cfd554c6
SumitSahu444 <sumitkumarsahu141@gmail.com>
2024-11-17 13:26:44 +0530

    Basic Nav layout done

---
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { IoSearchSharp } from "react-icons/io5";
+import { FiShoppingBag } from "react-icons/fi";
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className='nav-container flex justify-between align-middle'>
+            <div className='w-[150px]'>
+                <img src="images/logo.png" />
+            </div>
+            <div>
+                 <ul className='flex justify-between gap-12 align-middle'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Signup</li>
+                 </ul>
+            </div>
+            <div className='search-input-box flex px-4 py-2'>
+                <IoSearchSharp />
+                 <input type="search" className='pl-2 w-[130px] border-r border-black mr-2' placeholder='search' />
+                 <FiShoppingBag />
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
